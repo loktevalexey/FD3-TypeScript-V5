51a04fa7c9364788a1c7b6503e9b4dd731de4734
loktev <loktev@tut.by>
2019-03-10 14:14:39 +0300

    примеры на typeof

--- A_TS0-TS1/TS070_types_hash/app.ts
+++ A_TS0-TS1/TS070_types_hash/app.ts
@@ -2,3 +2,8 @@ var friend:{fam:string,im:string,otch:string,age:number}
     ={fam:"Иванов",im:"Иван",otch:"Иванович",age:25};
 
 console.log(friend);
+
+
+var friendKey:keyof {fam:string,im:string,otch:string,age:number};
+friendKey="fam";
+// friendKey="fam2"; // ошибка
--- A_TS0-TS1/TS110_types_type/app.ts
+++ A_TS0-TS1/TS110_types_type/app.ts
@@ -3,3 +3,13 @@ type StringList=Array<string>;
 
 var sl1:StringList=["a","b","c"];
 var sl2:StringList=sl1;
+
+
+type Man={fam:string,im:string,otch:string,age:number};
+
+var friend:Man
+    ={fam:"Иванов",im:"Иван",otch:"Иванович",age:25};
+
+var friendKey:keyof Man;
+friendKey="fam";
+//friendKey="fam2"; // ошибка
